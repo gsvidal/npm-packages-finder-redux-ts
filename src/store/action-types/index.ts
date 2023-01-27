23647595906a869766e4e76c510748a7d19ef248
Gonzalo Vidal <gonzalovidal.dev@gmail.com>
2023-01-27 16:26:01 -0500

    Create redux store with a repositories slice, actions and actions-types files

---
+++ src/store/action-types/index.ts
@@ -0,0 +1,6 @@
+//We will use string based enum to create a set of named constants or distinct/mutually exclusive cases
+export enum ActionType {
+  SEARCH_REPOSITORIES = 'search_repositories',
+  SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success',
+  SEARCH_REPOSITORIES_ERROR = 'search_repositories_error',
+}
